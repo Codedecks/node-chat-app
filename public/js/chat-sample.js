fa898fde33f7cedc67c1908f0c6810250d72dcc2
codedecks <codedecks@gmail.com>
2017-12-08 16:23:20 -0800

    Send message to people in same group

---
+++ public/js/chat-sample.js
@@ -0,0 +1,141 @@
+var socket = io();
+
+function scrollToBottom() {
+  // selector
+  var messages = $('#messages');
+  var newMessages = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessagesHeight = newMessages.innerHeight();
+  var lastMessagesHeight = newMessages.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessagesHeight + lastMessagesHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+socket.on('connect', function() {
+  // console.log('connected to server');
+
+  var params = $.deparam(window.location.search);
+
+  socket.emit('join', params, function(err) {
+    if(err){
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('no error');
+    }
+  });
+
+  // socket.emit('createEmail', {
+  //   to: 'jane@mail.com',
+  //   text: 'Hi Jane this is Brandon'
+  // });
+
+  // socket.emit('createMessage', {
+  //   from: 'jane',
+  //   text: 'Hi Jane this is Brandon'
+  // });
+});
+
+socket.on('disconnect', function() {
+  console.log('disconnected from server');
+});
+
+socket.on('updateUserList', function(users) {
+  var ol = $('<ol></ol>');
+
+  users.forEach(function(user) {
+    ol.append($('<li></li>').text(user));
+  });
+
+  $('#users').html(ol);
+});
+
+// socket.on('newEmail', function(email) {
+//   console.log('new email', email);
+// });
+
+socket.on('newMessage', function(message) {
+  // console.log('new message', message);
+
+  // var li = $('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  //
+  // $('#messages').append(li);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(html);
+  scrollToBottom();
+});
+
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi'
+// }, function(data) {
+//   console.log('Got it', data);
+// });
+
+socket.on('newLocationMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = $('<li></li>');
+  // var a = $('<a target="_blank">My current location</a>');
+  //
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  //
+  // $('#messages').append(li);
+  var template = $('#locationMessage-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(html);
+  scrollToBottom();
+});
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  var messageTextbox = $('[name=message]');
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('');
+  });
+});
+
+var locationButton = $('#send-location');
+  locationButton.on('click', function() {
+  if(!navigator.geolocation){
+    return alert('Geolocation not supported by your browser');
+  }
+
+  locationButton.attr('disabled', true).text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // console.log(position);
+    locationButton.removeAttr('disabled').text('Sending location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    locationButton.removeAttr('disabled').text('Sending location');
+    alert('Unable to fetch location.');
+  });
+});
--- server/server.js
+++ server/server.js
@@ -38,20 +38,21 @@ io.on('connection', (socket) => {
 
   // socket.emit to a single connection
   socket.on('createMessage', (message, callback) => {
-    console.log('create message', message);
-    // *** io.emit to all connections
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+
+    if(user && isRealString(message.text)) {
+      // *** io.emit to all connections
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
     callback();
-    // *** broadcast a single to everyone but itself
-      // socket.broadcast.emit('newMessage', {
-      //   from: message.from,
-      //   text: message.text,
-      //   createdAt: new Date().getTime()
-      // });
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
